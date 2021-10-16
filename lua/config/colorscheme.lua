47eb894a9aada235cdb231f2bc17cada11eb304f
Andrea Finocchio <finocchio.andrea94@gmail.com>
2021-10-16 11:45:23 +0100

    complete

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -7,6 +7,6 @@ vim.cmd [[
 let g:gruvbox_material_enable_bold = 1
 let g:gruvbox_material_enable_italic = 1
 let g:gruvbox_material_background = 'medium'
-let g:gruvbox_material_palette = 'mix'
+let g:gruvbox_material_palette = 'material'
 colorscheme gruvbox-material
 ]]
