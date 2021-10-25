b87478fd612cb3d8735a5a838fc6323638f87b76
Andrea Finocchio <finocchio.andrea94@gmail.com>
2021-10-25 12:43:25 +0100

    modified:   lua/config/completion.lua 	modified:   lua/settings.lua

--- lua/config/completion.lua
+++ lua/config/completion.lua
@@ -1,7 +1,6 @@
 local utils = require('utils')
 
---utils.opt('o', 'completeopt','menuone,noinsert,noselect,preview')
-vim.cmd [[set completeopt=menu,menuone,noinsert,noselect]]
+utils.opt('o', 'completeopt','menuone,noinsert,noselect')
 vim.cmd [[set shortmess+=c]]
 vim.g.completion_confirm_key = ""
 vim.g.completion_matching_strategy_list = {'exact', 'substring', 'fuzzy'} -- <Tab> to navigate the completion menu
