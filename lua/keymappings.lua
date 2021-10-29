3d09576cb0596906aa41c7e84960a3be10f5cc75
Andrea Finocchio <finocchio.andrea94@gmail.com>
2021-10-29 15:21:35 +0100

    modified:   lua/keymappings.lua

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -11,5 +11,7 @@ vim.cmd [[let mapleader=" "]]
 -- Compile and run with g++
 local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 local opts = { noremap=true, silent=true }
-utils.map('n', '<C-c>', ':!gcc  -o  %:r.o % && ./%:r.o<Enter>', opts)
-utils.map('n', '<C-p>', ':!g++  -std=c++14 -o  %:r.o % && ./%:r.o<Enter>', opts)
+utils.map('n', '<C-m>c', ':!gcc  -o  %:r.o % && ./%:r.o<Enter>', opts)
+utils.map('n', '<C-m>p', ':!g++  -std=c++14 -o  %:r.o % && ./%:r.o<Enter>', opts)
+utils.map('n', '<C-m><C-m>', ':!cmake . && make<Enter>', opts)
+utils.map('t', '<leader><Esc>', '<C-\\><C-n> <C-w><C-w>', opts)
