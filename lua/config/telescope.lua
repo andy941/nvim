3eac28329da38d1439c4016b6ba4b39da167e7ed
andy941 <finocchio.andrea94@gmail.com>
2021-11-29 23:05:45 +0000

    telescope panes size increased

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -3,23 +3,28 @@ local utils = require('utils')
 -- You dont need to set any of these options. These are the default ones. Only
 -- the loading is important
 require('telescope').setup {
-  extensions = {
-    fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
-    }
-  },
-  defaults = {
-	  file_ignore_patterns = { 
-		  'Pictures',
-		  'Drives',
-		'vpnconfigs',
+	extensions = {
+		fzf = {
+			fuzzy = true,                    -- false will only do exact matching
+			override_generic_sorter = true,  -- override the generic sorter
+			override_file_sorter = true,     -- override the file sorter
+			case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+			-- the default case_mode is "smart_case"
+		}
+	},
+	defaults = {
+		file_ignore_patterns = { 
+			'Pictures',
+			'Drives',
+			'vpnconfigs',
 			'.git'
-	  }
-  }
+		},
+
+		layout_config = {
+			width = 0.95,
+			height = 0.95
+		}
+	}
 }
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
