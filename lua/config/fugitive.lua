e2bc7b5f5e017f21e074109b2aeb132bdb5ce981
andy941 <finocchio.andrea94@gmail.com>
2021-12-03 22:20:10 +0000

    Fugitive keymaps

--- lua/config/fugitive.lua
+++ lua/config/fugitive.lua
@@ -1,1 +1,4 @@
-local utils = require('utils')utils.map('n', '<Leader>gs', '<cmd>Gstatus<CR>')  -- Git status
+local utils = require('utils')utils.map('n', '<Leader>G', '<cmd>Gstatus<CR>')  -- Git status
+local utils = require('utils')utils.map('n', '<Leader>M', '<cmd>Gvdiffsplit!<CR>')  -- Git diff
+local utils = require('utils')utils.map('n', '<Leader>hh', '<cmd>diffget //2<CR>')  -- Git diffget left
+local utils = require('utils')utils.map('n', '<Leader>ll', '<cmd>diffget //3<CR>')  -- Git diffget right
--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,7 +1,6 @@
 local utils = require('utils')
 
 utils.map('n', '<CR>', '<cmd>noh<CR>') -- Clear highlights
-utils.map('n', '<CR>', '<cmd>noh<CR>') -- Clear highlights
 
 ---- quickfix list FIX 
 vim.cmd [[autocmd BufReadPost quickfix nnoremap <buffer> <CR> <CR>]]
@@ -12,6 +11,11 @@ vim.cmd [[let mapleader=" "]]
 local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 local opts = { noremap=true, silent=true }
 -- utils.map('n', '<C-m>c', ':!gcc  -o  %:r.o % && ./%:r.o<Enter>', opts)
---utils.map('n', '<C-m>p', ':!g++  -std=c++14 ~/Bioinformatics_Algorithms/lib/Tools.cpp -o  %:r.o % && ./%:r.o<Enter>', opts)
-utils.map('n', '<C-m><C-m>', ':!cmake --build build -j$(getconf _NPROCESSORS_ONLN) && cmake --install build && ./%:r.o<Enter>', opts)
+-- utils.map('n', '<C-m>p', ':!g++  -std=c++14 ~/Bioinformatics_Algorithms/lib/Tools.cpp -o  %:r.o % && ./%:r.o<Enter>', opts)
+-- utils.map('n', '<C-m><C-m>', ':!cmake --build build -j$(getconf _NPROCESSORS_ONLN) && cmake --install build && ./%:r.o<Enter>', opts)
+utils.map('t', '<leader><Esc>', '<C-\\><C-n> <C-w><C-w>', opts)
+
+-- Fugitive
+utils.map('t', '<leader><Esc>', '<C-\\><C-n> <C-w><C-w>', opts)
+utils.map('t', '<leader><Esc>', '<C-\\><C-n> <C-w><C-w>', opts)
 utils.map('t', '<leader><Esc>', '<C-\\><C-n> <C-w><C-w>', opts)
