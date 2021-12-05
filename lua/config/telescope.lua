eca2df708c60c7dbcf23b78995abf0dd01f41f1e
andy941 <finocchio.andrea94@gmail.com>
2021-12-05 01:40:26 +0000

    telescope colorscheme keybind

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -43,15 +43,17 @@ require('telescope').setup {
 require('telescope').load_extension('fzf')
 
 -- KeyBindings
-utils.map('n', '<leader>f', '<cmd>Telescope find_files<cr>')
-utils.map('n', '<leader>g', '<cmd>Telescope live_grep<cr>')
-utils.map('n', '<leader>t', '<cmd>Telescope treesitter<cr>')
-utils.map('n', '<leader>b', '<cmd>Telescope buffers<cr>')
-utils.map('n', '<leader>e', '<cmd>Telescope file_browser<cr>')
-utils.map('n', '<leader>ht', '<cmd>Telescope help_tags<cr>')
+utils.map('n', '<leader>f',		'<cmd>Telescope		find_files<cr>')
+utils.map('n', '<leader>gg',  	'<cmd>Telescope 	live_grep<cr>')
+utils.map('n', '<leader>tt',  	'<cmd>Telescope 	treesitter<cr>')
+utils.map('n', '<leader>b',   	'<cmd>Telescope 	buffers<cr>')
+utils.map('n', '<leader>ee',  	'<cmd>Telescope 	file_browser<cr>')
+utils.map('n', '<leader>ht',  	'<cmd>Telescope 	help_tags<cr>')
 
-utils.map('n', '<leader>wf', '<cmd>Telescope find_files cwd=%:p:h<cr>')
-utils.map('n', '<leader>wg', '<cmd>Telescope live_grep cwd=%:p:h<cr>')
-utils.map('n', '<leader>we', '<cmd>Telescope file_browser cwd=%:p:h<cr>')
+utils.map('n', '<leader>wf',  	'<cmd>Telescope 	find_files			cwd=%:p:h<cr>')
+utils.map('n', '<leader>wg',  	'<cmd>Telescope 	live_grep			cwd=%:p:h<cr>')
+utils.map('n', '<leader>we',  	'<cmd>Telescope 	file_browser		cwd=%:p:h<cr>')
 
-utils.map('n', '<leader>rc', '<cmd>Telescope find_files cwd=~/.config/nvim<cr>')    -- go to neovim directory for quick config changes
+utils.map('n', '<leader>rc',  	'<cmd>Telescope 	find_files			cwd=~/.config/nvim<cr>')    -- go to neovim directory for quick config changes
+utils.map('n', '<leader>col', 	'<cmd>Telescope 	colorscheme<cr>')    -- go to neovim directory for quick config changes
+
