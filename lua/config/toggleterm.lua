f9f683a00e4fa3995e1c4fa7f7104ccc564a730b
Andrea Finocchio <finocchio.andrea94@gmail.com>
2022-02-07 18:50:54 +0000

    Bufferlne: added full defaults and small tweaks Bufferlne: Keymaps for bufferline buffer change

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -5,7 +5,6 @@ require("toggleterm").setup{
 	size = function(term)
 		if term.direction == "horizontal" then
 			return vim.o.lines * 0.3
-			--return 15
 		elseif term.direction == "vertical" then
 			return vim.o.columns * 0.3
 		end
