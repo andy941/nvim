5ec542075c301bf716d91826520776a4c5802b8c
Andrea Finocchio <finocchio.andrea94@gmail.com>
2022-03-11 14:47:08 +0000

    nvim-dap just call debugger, hope is in PATH

--- lua/config/nvim-dap.lua
+++ lua/config/nvim-dap.lua
@@ -1,7 +1,7 @@
 local dap = require('dap')
 dap.adapters.lldb = {
   type = 'executable',
-  command = '/usr/bin/lldb-vscode', -- adjust as needed
+  command = 'lldb-vscode', -- adjust as needed
   name = "lldb",
 }
 dap.configurations.cpp = {
