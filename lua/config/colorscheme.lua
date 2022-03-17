5d943a5cb6c18932f97d1790bc8dfd5600c2d5aa
Andrea Finocchio <finocchio.andrea94@gmail.com>
2022-03-17 14:21:15 +0000

    nightfox fix after rewriting

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,26 +1,37 @@
-local utils = require('utils')
+local util = require('packer.util')
 
 vim.o.termguicolors = true
 vim.o.background = "dark" -- or "light" for light mode
 
-local nightfox = require('nightfox')
--- This function set the configuration of nightfox. If a value is not passed in the setup function
--- it will be taken from the default configuration above
-nightfox.setup({
-	fox = "nightfox", -- change the colorscheme to use nordfox
-	styles = {
-		comments = "italic", -- change style of comments to be italic
-		keywords = "bold", -- change style of keywords to be bold
-		functions = "italic" -- styles can be a comma separated list
-	},
-	inverse = {
-		match_paren = true, -- inverse the highlighting of match_parens
-		visual = true,
-		search = true
-	},
-	colors = {},
-	hlgroups = {}
+-- Default options
+require('nightfox').setup({
+  options = {
+    -- Compiled file's destination location
+    compile_path = util.join_paths(vim.fn.stdpath("cache"), "nightfox"),
+    compile_file_suffix = "_compiled", -- Compiled file suffix
+    transparent = false,    -- Disable setting background
+    terminal_colors = false, -- Set terminal colors (vim.g.terminal_color_*)
+    dim_inactive = false,   -- Non focused panes set to alternative background
+		styles = {              -- Style to be applied to different syntax groups
+			comments = "italic", -- change style of comments to be italic
+			keywords = "bold", -- change style of keywords to be bold
+			functions = "italic", -- styles can be a comma separated list
+			numbers = "NONE",
+			strings = "NONE",
+			types = "NONE",
+			variables = "NONE",
+		},
+    inverse = {             -- Inverse highlight for different types
+      match_paren = true,
+      visual = true,
+      search = true,
+    },
+    modules = {             -- List of various plugins and additional options
+      -- ...
+    },
+  }
 })
--- Load the configuration set above and apply the colorscheme
-nightfox.load()
+
+-- setup must be called before loading
+vim.cmd("colorscheme nightfox")
 
