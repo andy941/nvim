e81cd2c54fb09e15d60de71800c7e82326e03355
Andrea Finocchio <finocchio.andrea94@gmail.com>
2022-04-27 13:26:11 +0100

    Better cmake compile keybindings

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -3,12 +3,17 @@ local utils = require('utils')
 local opts = { noremap=true, silent=true }
 
 utils.map("n", -- Release
-	"<leader>M",
-	[[:2TermExec cmd="cd build/ && cmake -DCMAKE_BUILD_TYPE=Release .. && cd .. && cmake --build build -j$(getconf _NPROCESSORS_ONLN) && cmake --install build && %:r.o" <CR>]],
+	"<leader>Mr",
+	[[:TermExec cmd="rm -r build; mkdir build; cd build/ && cmake -DCMAKE_BUILD_TYPE=Release .. && cd .. && cmake --build build -j$(getconf _NPROCESSORS_ONLN) && cmake --install build && %:r.o" <CR>]],
+	opts)
+
+utils.map("n", -- Debug
+	"<leader>Md",
+	[[:TermExec cmd="rm -r build; mkdir build; cd build/ && cmake -DCMAKE_BUILD_TYPE=Debug .. && cd .. && cmake --build build -j$(getconf _NPROCESSORS_ONLN) && cmake --install build && %:r.o" <CR>]],
 	opts)
 
 utils.map("n",
-	"<leader>m",
-	[[:2TermExec cmd="cmake --build build -j$(getconf _NPROCESSORS_ONLN) && cmake --install build && %:r.o" <CR>]],
+	"<leader>MM",
+	[[:TermExec cmd="cmake --build build -j$(getconf _NPROCESSORS_ONLN) && cmake --install build && %:r.o" <CR>]],
 	opts)
 
