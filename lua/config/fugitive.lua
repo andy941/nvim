277996b81e414311aa330233d075c384eda0dc9c
Andrea Finocchio <finocchio.andrea94@gmail.com>
2022-04-28 11:32:56 +0100

    solve keybind clash

--- lua/config/fugitive.lua
+++ lua/config/fugitive.lua
@@ -1,3 +1,4 @@
-local utils = require('utils')utils.map('n', '<leader>M', '<cmd>Gvdiffsplit!<CR>')  -- Git diff
-local utils = require('utils')utils.map('n', '<leader>hh', '<cmd>diffget //2<CR>')  -- Git diffget left
-local utils = require('utils')utils.map('n', '<leader>ll', '<cmd>diffget //3<CR>')  -- Git diffget right
+local utils = require('utils')
+utils.map('n', '<leader>dv', '<cmd>Gvdiffsplit!<CR>')  -- Git diff
+utils.map('n', '<leader>hh', '<cmd>diffget //2<CR>')   -- Git diffget left
+utils.map('n', '<leader>ll', '<cmd>diffget //3<CR>')   -- Git diffget right
