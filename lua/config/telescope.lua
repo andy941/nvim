386e1f16a7d833d34a951c7b8642582ae27b253d
Andrea Finocchio <finocchio.andrea94@gmail.com>
2022-04-29 12:59:05 +0100

    small telescope refactoring

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,10 +1,11 @@
 local utils = require('utils')
 local opts = { noremap=true, silent=true }
-local actions = require "telescope.actions"
+local tele = require('telescope')
+local actions = require('telescope.actions')
 
 -- You dont need to set any of these options. These are the default ones. Only
 -- the loading is important
-require('telescope').setup {
+tele.setup {
 	defaults = {
 		file_ignore_patterns = { -- % is an escape char in lua regex
 			'Pictures/',
@@ -14,8 +15,8 @@ require('telescope').setup {
 			'lua%-language%-server/',
 		},
 		layout_config = {
-			width = 0.99,
-			height = 0.99,
+			width = 0.90,
+			height = 0.95,
 			preview_width = 0.50
 		},
 		mappings = {
@@ -48,23 +49,23 @@ require('telescope').setup {
 			-- the default case_mode is "smart_case"
 		},
 		bibtex = {
-      -- Context awareness disabled by default
-      context = true,
-      -- Fallback to global/directory .bib files if context not found
-      -- This setting has no effect if context = false
-      context_fallback = true
-    },
+			-- Context awareness disabled by default
+			context = true,
+			-- Fallback to global/directory .bib files if context not found
+			-- This setting has no effect if context = false
+			context_fallback = true
+		},
 	},
 }
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
-require('telescope').load_extension('fzf')
+tele.load_extension('fzf')
 
 -- project management with Telescope
-require('telescope').load_extension('project')
+tele.load_extension('project')
 
 -- Bibtex citation search
-require('telescope').load_extension('bibtex')
+tele.load_extension('bibtex')
 
 -- KeyBindings -----------------------------------------------------------------------
 
