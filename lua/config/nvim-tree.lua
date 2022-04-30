ba498a82cbce7abe2a93cad6063dff64924725e4
Andrea Finocchio <finoccha@tcd.ie>
2022-04-30 16:39:01 +0100

    nvim-tree typo correction

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -9,68 +9,48 @@ local lib = require("nvim-tree.lib")
 local view = require("nvim-tree.view")
 
 local function collapse_all()
-    require("nvim-tree.actions.collapse-all").fn()
-end
-
-local function edit_or_open()
-    -- open as vsplit on current node
-    local action = "edit"
-    local node = lib.get_node_at_cursor()
-
-    -- Just copy what's done normally with vsplit
-    if node.link_to and not node.nodes then
-        require('nvim-tree.actions.open-file').fn(action, node.link_to)
-        view.close() -- Close the tree if file was opened
-
-    elseif node.nodes ~= nil then
-        lib.expand_or_collapse(node)
-
-    else
-        require('nvim-tree.actions.open-file').fn(action, node.absolute_path)
-        view.close() -- Close the tree if file was opened
-    end
-
+	require("nvim-tree.actions.collapse-all").fn()
 end
 
 local function vsplit_preview()
-    -- open as vsplit on current node
-    local action = "vsplit"
-    local node = lib.get_node_at_cursor()
+	-- open as vsplit on current node
+	local action = "vsplit"
+	local node = lib.get_node_at_cursor()
 
-    -- Just copy what's done normally with vsplit
-    if node.link_to and not node.nodes then
-        require('nvim-tree.actions.open-file').fn(action, node.link_to)
+	-- Just copy what's done normally with vsplit
+	if node.link_to and not node.nodes then
+		require('nvim-tree.actions.open-file').fn(action, node.link_to)
 
-    elseif node.nodes ~= nil then
-        lib.expand_or_collapse(node)
+	elseif node.nodes ~= nil then
+		lib.expand_or_collapse(node)
 
-    else
-        require('nvim-tree.actions.open-file').fn(action, node.absolute_path)
+	else
+		require('nvim-tree.actions.open-file').fn(action, node.absolute_path)
 
-    end
+	end
 
-    -- Finally refocus on tree if it was lost
-    view.focus()
+	-- Finally refocus on tree if it was lost
+	view.focus()
 end
 
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
-  auto_reload_on_write = true,
-  disable_netrw = false,
-  hide_root_folder = false,
-  hijack_cursor = true,
-  hijack_netrw = true,
-  hijack_unnamed_buffer_when_opening = true,
-  ignore_buffer_on_setup = false,
-  open_on_setup = false,
-  open_on_setup_file = false,
-  open_on_tab = false,
-  sort_by = "name",
-  update_cwd = true,
-  view = {
+	auto_reload_on_write = true,
+	disable_netrw = false,
+	hijack_cursor = true,
+	hijack_netrw = true,
+	hijack_unnamed_buffer_when_opening = true,
+	ignore_buffer_on_setup = false,
+	open_on_setup = false,
+	open_on_setup_file = false,
+	open_on_tab = false,
+	sort_by = "name",
+	update_cwd = true,
+	view = {
 		width = 30,
 		height = 30,
+		hide_root_folder = false,
 		side = "left",
 		preserve_window_proportions = true,
 		number = false,
@@ -79,95 +59,96 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
 		mappings = {
 			custom_only = false,
 			list = {
-				{ key = "l", action = "edit", action_cb = edit_or_open },
+				{ key = "l", action = "open" },
 				{ key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
 				{ key = "h", action = "close_node" },
 				{ key = "H", action = "collapse_all", action_cb = collapse_all }
-			}
+			},
 		},
-		renderer = {
-			indent_markers = {
-				enable = true,
-				icons = {
-					corner = "└ ",
-					edge = "│ ",
-					none = "  ",
+	},
+
+	renderer = {
+		indent_markers = {
+			enable = true,
+			icons = {
+				corner = "└ ",
+				edge = "│ ",
+				none = "  ",
+			},
+		},
+		icons = {
+			webdev_colors = true,
+		},
+	},
+	hijack_directories = {
+		enable = true,
+		auto_open = true,
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = false,
+		ignore_list = {},
+	},
+	ignore_ft_on_setup = {},
+	system_open = {
+		cmd = nil,
+		args = {},
+	},
+	diagnostics = {
+		enable = false,
+		show_on_dirs = false,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	filters = {
+		dotfiles = false,
+		custom = {},
+		exclude = {},
+	},
+	git = {
+		enable = true,
+		ignore = true,
+		timeout = 400,
+	},
+	actions = {
+		use_system_clipboard = true,
+		change_dir = {
+			enable = true,
+			global = false,
+			restrict_above_cwd = false,
+		},
+		open_file = {
+			quit_on_open = false,
+			resize_window = false,
+			window_picker = {
+				enable = false,
+				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+				exclude = {
+					filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+					buftype = { "nofile", "terminal", "help" },
 				},
 			},
-			icons = {
-				webdev_colors = true,
-			},
 		},
-		hijack_directories = {
-			enable = true,
-			auto_open = true,
+	},
+	trash = {
+		cmd = "trash",
+		require_confirm = true,
+	},
+	log = {
+		enable = false,
+		truncate = false,
+		types = {
+			all = false,
+			config = false,
+			copy_paste = false,
+			diagnostics = false,
+			git = false,
+			profile = false,
 		},
-		update_focused_file = {
-			enable = true,
-			update_cwd = false,
-			ignore_list = {},
-		},
-		ignore_ft_on_setup = {},
-		system_open = {
-			cmd = nil,
-			args = {},
-		},
-		diagnostics = {
-			enable = false,
-			show_on_dirs = false,
-			icons = {
-				hint = "",
-				info = "",
-				warning = "",
-				error = "",
-			},
-		},
-		filters = {
-			dotfiles = false,
-			custom = {},
-			exclude = {},
-		},
-		git = {
-			enable = true,
-			ignore = true,
-			timeout = 400,
-		},
-		actions = {
-			use_system_clipboard = true,
-			change_dir = {
-				enable = true,
-				global = false,
-				restrict_above_cwd = false,
-			},
-			open_file = {
-				quit_on_open = false,
-				resize_window = false,
-				window_picker = {
-					enable = false,
-					chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-					exclude = {
-						filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-						buftype = { "nofile", "terminal", "help" },
-					},
-				},
-			},
-		},
-		trash = {
-			cmd = "trash",
-			require_confirm = true,
-		},
-		log = {
-			enable = false,
-			truncate = false,
-			types = {
-				all = false,
-				config = false,
-				copy_paste = false,
-				diagnostics = false,
-				git = false,
-				profile = false,
-			},
-		},
-	}
+	},
 } -- END_DEFAULT_OPTS
 
