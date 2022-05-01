aa10326f2e6523a50c31e672c3275a4f05fd82dc
andy941 <finocchio.andrea94@gmail.com>
2022-05-02 00:16:15 +0100

    keybind change completion

--- lua/config/completion.lua
+++ lua/config/completion.lua
@@ -35,7 +35,7 @@ cmp.setup({
 	mapping = {
 		['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
 		['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-		['<C-d>'] = cmp.mapping.scroll_docs(-4),
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.close(),
