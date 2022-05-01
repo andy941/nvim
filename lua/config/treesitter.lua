45e90c5805a7593b1ad1c32056ff200de80cf62b
andy941 <finocchio.andrea94@gmail.com>
2022-05-02 00:15:26 +0100

    treesitter ignore phpdoc (install errors)

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,8 +1,8 @@
-local utils = require('utils')
-
-
 require'nvim-treesitter.configs'.setup {
   ensure_installed = "all",
+	ignore_install = {
+		"phpdoc"
+	},
   highlight = {
     enable = true,              -- false will disable the whole extension
 	additional_vim_regex_highlighting = false,
