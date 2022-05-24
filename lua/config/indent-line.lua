60d73f833782c630f05156ca4581f4cc0230e011
Andrea Finocchio <finocchio.andrea94@gmail.com>
2022-05-24 21:39:11 +0100

    indent line don't show context start underline

--- lua/config/indent-line.lua
+++ lua/config/indent-line.lua
@@ -1,7 +1,7 @@
 require("indent_blankline").setup {
 	space_char_blankline = " ",
 	show_current_context = true,
-	show_current_context_start = true,
+	show_current_context_start = false,
 	use_treesitter = true,
 	use_treesitter_scope = true,
 	max_indent_increase = 1,
