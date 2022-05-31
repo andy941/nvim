dcfdd6065de4ad1f63a9952eb9b947b212013713
Andrea Finocchio <finocchio.andrea94@gmail.com>
2022-05-31 16:47:35 +0100

    explicit colorscheme nightfox modules enabling

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -11,7 +11,7 @@ require('nightfox').setup({
     compile_file_suffix = "_compiled", -- Compiled file suffix
     transparent = false,    -- Disable setting background
     terminal_colors = false, -- Set terminal colors (vim.g.terminal_color_*)
-    dim_inactive = true,   -- Non focused panes set to alternative background
+    dim_inactive = false,   -- Non focused panes set to alternative background
 		styles = {              -- Style to be applied to different syntax groups
 			comments = "italic", -- change style of comments to be italic
 			keywords = "bold", -- change style of keywords to be bold
@@ -26,8 +26,36 @@ require('nightfox').setup({
       visual = true,
       search = true,
     },
-    modules = {             -- List of various plugins and additional options
-      -- ...
+		modules = {             -- List of various plugins and additional options
+			aerial = true,
+			barbar= true,
+			cmp= true,
+			dap_ui= true,
+			dashboard= true,
+			diagnostic= true,
+			fern= true,
+			fidget= true,
+			gitgutter= true,
+			gitsigns= true,
+			glyph_palette= true,
+			hop= true,
+			illuminate= true,
+			lightspeed= true,
+			lsp_saga= true,
+			lsp_trouble= true,
+			modes= true,
+			native_lsp= true,
+			neogit= true,
+			neotree= true,
+			notify= true,
+			nvimtree= true,
+			pounce= true,
+			sneak= true,
+			symbol_outline= true,
+			telescope= true,
+			treesitter= true,
+			tsrainbow= true,
+			whichkey= true,
     },
   }
 })
