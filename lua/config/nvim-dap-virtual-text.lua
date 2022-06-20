702e88d52b27339e4f4e8734a880f67d9254ce47
andy941 <finocchio.andrea94@gmail.com>
2022-06-20 21:17:20 +0100

    compatibility fixes

--- lua/config/nvim-dap-virtual-text.lua
+++ lua/config/nvim-dap-virtual-text.lua
@@ -6,7 +6,7 @@ require("nvim-dap-virtual-text").setup {
     show_stop_reason = true,               -- show stop reason when stopped for exceptions
     commented = false,                     -- prefix virtual text with comment string
     only_first_definition = true,          -- only show virtual text at first definition (if there are multiple)
-    all_references = true,                 -- show virtual text on all all references of the variable (not only definitions)
+    all_references = false,                 -- show virtual text on all all references of the variable (not only definitions)
     filter_references_pattern = '<module', -- filter references (not definitions) pattern when all_references is activated (Lua gmatch pattern, default filters out Python modules)
     -- experimental features:
     virt_text_pos = 'eol',                 -- position of virtual text, see `:h nvim_buf_set_extmark()`
