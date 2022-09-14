efa4979423e7d323d614863f59bce8355560ce7e
andy941 <finocchio.andrea94@gmail.com>
2022-09-14 15:09:15 +0100

    clangd tools

--- lua/config/completion.lua
+++ lua/config/completion.lua
@@ -1,54 +1,54 @@
-local utils = require('utils')
+local utils = require("utils")
 
-utils.opt('o', 'completeopt','menu,menuone,noselect')
-vim.cmd [[set shortmess+=c]]
+utils.opt("o", "completeopt", "menu,menuone,noselect")
+vim.cmd([[set shortmess+=c]])
 vim.g.completion_confirm_key = ""
-vim.g.completion_matching_strategy_list = {'exact', 'substring', 'fuzzy'} -- <Tab> to navigate the completion menu
-
+vim.g.completion_matching_strategy_list = { "exact", "substring", "fuzzy" } -- <Tab> to navigate the completion menu
 
 -- Vsnip functions from https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings
 local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 -- Setup nvim-cmp.
-local luasnip = require('luasnip')
-local cmp = require('cmp')
+local luasnip = require("luasnip")
+local cmp = require("cmp")
 
 cmp.setup({
 	formatting = {
-		format = require('lspkind').cmp_format({
+		format = require("lspkind").cmp_format({
 			--with_text = true,
-			mode = 'symbol_text',
+			mode = "symbol_text",
 			--menu = ({}),
-			preset = 'default',
-			maxwidth = 50})
+			preset = "default",
+			maxwidth = 50,
+		}),
 	},
 
 	snippet = {
 		expand = function(args)
 			-- For `luasnip` user.
-			require('luasnip').lsp_expand(args.body)
+			require("luasnip").lsp_expand(args.body)
 		end,
 	},
 
 	mapping = {
-		['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-		['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.close(),
-		['<CR>'] = cmp.mapping.confirm({
+		["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.close(),
+		["<CR>"] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Replace,
-			select = true
+			select = true,
 		}),
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
 			elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
+				luasnip.expand_or_jump()
 			elseif has_words_before() then
 				cmp.complete()
 			else
@@ -60,28 +60,39 @@ cmp.setup({
 			if cmp.visible() then
 				cmp.select_prev_item()
 			elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
+				luasnip.jump(-1)
 			end
-		end, { "i", "s" })
+		end, { "i", "s" }),
 	},
 
 	experimental = {
-      ghost_text = true,
-    },
+		ghost_text = true,
+	},
 
-	enabled = function ()
-    return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt"
-      or require("cmp_dap").is_dap_buffer()
-  end,
+	enabled = function()
+		return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
+	end,
 
 	sources = {
-		{ name = 'dap' },
-		{ name = 'path' },
-		{ name = 'nvim_lsp' },
-		{ name = 'nvim_lsp_signature_help' },
-		{ name = 'nvim_lua' }, -- knows to activate only in Lua
-		{ name = 'luasnip' },
-		{ name = 'cmdline'},
+		{ name = "dap" },
+		{ name = "path" },
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lsp_signature_help" },
+		{ name = "nvim_lua" }, -- knows to activate only in Lua
+		{ name = "luasnip" },
+		{ name = "cmdline" },
 		--{ name = 'buffer'},
-	}
+	},
+	sorting = {
+		comparators = {
+			cmp.config.compare.offset,
+			cmp.config.compare.exact,
+			cmp.config.compare.recently_used,
+			require("clangd_extensions.cmp_scores"),
+			cmp.config.compare.kind,
+			cmp.config.compare.sort_text,
+			cmp.config.compare.length,
+			cmp.config.compare.order,
+		},
+	},
 })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,107 +1,102 @@
-return require('packer').startup(function()
-
+return require("packer").startup(function()
 	-- Packer can manage itself as an optional plugin
-	use { 'wbthomason/packer.nvim', opt = true }
+	use({ "wbthomason/packer.nvim", opt = true })
 
 	-- Some core plugins
-	use { 'nvim-lua/popup.nvim' }
-	use { 'nvim-lua/plenary.nvim' }
+	use({ "nvim-lua/popup.nvim" })
+	use({ "nvim-lua/plenary.nvim" })
 
 	-- Icons support
-	use { 'kyazdani42/nvim-web-devicons' }
-	use { 'mortepau/codicons.nvim' }
-
+	use({ "kyazdani42/nvim-web-devicons" })
+	use({ "mortepau/codicons.nvim" })
 
 	-- bufferline on top
-	use {'akinsho/bufferline.nvim',
-		requires = 'kyazdani42/nvim-web-devicons'}
+	use({ "akinsho/bufferline.nvim", requires = "kyazdani42/nvim-web-devicons" })
 
 	-- File Browser
-	use {'kyazdani42/nvim-tree.lua',
-		requires = 'kyazdani42/nvim-web-devicons'}
+	use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
 
 	-- Make it easier to use the terminal
-	use { 'akinsho/toggleterm.nvim' }
+	use({ "akinsho/toggleterm.nvim" })
 
 	-- Color scheme
-	use { 'ellisonleao/gruvbox.nvim' }
+	use({ "ellisonleao/gruvbox.nvim" })
 
 	-- Lualine
-	use { 'hoob3rt/lualine.nvim' }
+	use({ "hoob3rt/lualine.nvim" })
 
 	-- Fuzzy finder
-	use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-	use { 'nvim-telescope/telescope-project.nvim' }
-	use { 'nvim-telescope/telescope-bibtex.nvim' }
-	use { 'nvim-telescope/telescope-dap.nvim' }
-	use { 'crispgm/telescope-heading.nvim' }
-	use { 'nvim-telescope/telescope.nvim' }
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	use({ "nvim-telescope/telescope-project.nvim" })
+	use({ "nvim-telescope/telescope-bibtex.nvim" })
+	use({ "nvim-telescope/telescope-dap.nvim" })
+	use({ "crispgm/telescope-heading.nvim" })
+	use({ "nvim-telescope/telescope.nvim" })
 
 	-- LSP
-	use { 'neovim/nvim-lspconfig' }
+	use({ "neovim/nvim-lspconfig" })
+	use({ "p00f/clangd_extensions.nvim" })
 
 	-- Code formatting
-	use { 'mhartington/formatter.nvim' }
+	use({ "mhartington/formatter.nvim" })
 
 	-- Completion
-	use { 'hrsh7th/nvim-cmp' }
-	use { 	'hrsh7th/cmp-path' }
-	use { 	'hrsh7th/cmp-nvim-lsp' }
-	use { 	'hrsh7th/cmp-nvim-lsp-signature-help' }
-	use { 	'hrsh7th/cmp-nvim-lua' }
-	use { 	'hrsh7th/cmp-buffer' }
-	use { 	'hrsh7th/cmp-cmdline' }
-	use { 	'rcarriga/cmp-dap' }
-	use { 	'L3MON4D3/LuaSnip' }
-	use { 	'saadparwaiz1/cmp_luasnip' }
-	use { 	'onsails/lspkind-nvim' }
+	use({ "hrsh7th/nvim-cmp" })
+	use({ "hrsh7th/cmp-path" })
+	use({ "hrsh7th/cmp-nvim-lsp" })
+	use({ "hrsh7th/cmp-nvim-lsp-signature-help" })
+	use({ "hrsh7th/cmp-nvim-lua" })
+	use({ "hrsh7th/cmp-buffer" })
+	use({ "hrsh7th/cmp-cmdline" })
+	use({ "rcarriga/cmp-dap" })
+	use({ "L3MON4D3/LuaSnip" })
+	use({ "saadparwaiz1/cmp_luasnip" })
+	use({ "onsails/lspkind-nvim" })
 
 	-- Treesitter
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate'
-	}
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
 
 	-- Context aware spell checking
-	use { 'lewis6991/spellsitter.nvim'}
+	use({ "lewis6991/spellsitter.nvim" })
 
 	-- Auto close brackets
-	use { 'windwp/nvim-autopairs' }
+	use({ "windwp/nvim-autopairs" })
 
 	-- Rstudio like environment for R
-	use { 'jalvesaq/Nvim-R' , branch = 'stable'}
+	use({ "jalvesaq/Nvim-R", branch = "stable" })
 
 	-- resize buffers and keep proportions
-	use { 'kwkarlwang/bufresize.nvim' }
+	use({ "kwkarlwang/bufresize.nvim" })
 
 	-- Git workflow
-	use { 'tpope/vim-fugitive' }
-	use { 'lewis6991/gitsigns.nvim' }
+	use({ "tpope/vim-fugitive" })
+	use({ "lewis6991/gitsigns.nvim" })
 
 	-- Work with csv files and other tables easily
-	use { 'chrisbra/csv.vim' }
+	use({ "chrisbra/csv.vim" })
 
 	-- markdown preview
-	use { 'iamcco/markdown-preview.nvim', run = ':call mkdp#util#install()' }
+	use({ "iamcco/markdown-preview.nvim", run = ":call mkdp#util#install()" })
 
 	-- latex preview and more
-	use { 'lervag/vimtex' }
+	use({ "lervag/vimtex" })
 
 	-- Debugger API and sugar
-	use { 'mfussenegger/nvim-dap' }
-	use { 'rcarriga/nvim-dap-ui' }
-	use { 'theHamsta/nvim-dap-virtual-text' }
-	use { 'mfussenegger/nvim-dap-python' }
+	use({ "mfussenegger/nvim-dap" })
+	use({ "rcarriga/nvim-dap-ui" })
+	use({ "theHamsta/nvim-dap-virtual-text" })
+	use({ "mfussenegger/nvim-dap-python" })
 
 	-- Show hex colors in file
-	use { 'norcalli/nvim-colorizer.lua' }
+	use({ "norcalli/nvim-colorizer.lua" })
 
 	-- Zen mode
-	use { 'folke/zen-mode.nvim' }
-	use { 'Pocco81/true-zen.nvim' }
+	use({ "folke/zen-mode.nvim" })
+	use({ "Pocco81/true-zen.nvim" })
 
 	-- Show indent line and current context with treesitter
-	use { 'lukas-reineke/indent-blankline.nvim' }
-
-end
-)
+	use({ "lukas-reineke/indent-blankline.nvim" })
+end)
