639792affb247f09a9e24bdf2b6d4382140e7729
andy941 <finocchio.andrea94@gmail.com>
2022-09-14 15:39:35 +0100

    window management

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -95,8 +95,10 @@ return require("packer").startup(function()
 
 	-- Zen mode
 	use({ "folke/zen-mode.nvim" })
-	use({ "Pocco81/true-zen.nvim" })
 
 	-- Show indent line and current context with treesitter
 	use({ "lukas-reineke/indent-blankline.nvim" })
+
+	-- window management
+	use({ "sindrets/winshift.nvim" })
 end)
