45ef9f4958727f36c082ca14d96fb844d36fc09e
andy941 <finocchio.andrea94@gmail.com>
2022-10-31 15:48:24 +0000

    use Ctrl-L to clear highlight

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,16 +1,10 @@
-local utils = require('utils')
-local opts = { noremap=true, silent=true }
+local utils = require("utils")
+local opts = { noremap = true, silent = true }
 
 -- quickfix list FIX
-vim.cmd [[autocmd BufReadPost quickfix nnoremap <buffer> <CR> <CR>]]
-vim.cmd [[set wildchar=<Tab>]]
-vim.cmd [[let mapleader=" "]]
-
--- Clear highlights
-utils.map('n', '<CR>', '<cmd>noh<CR>', opts)
+vim.cmd([[autocmd BufReadPost quickfix nnoremap <buffer> <CR> <CR>]])
+vim.cmd([[set wildchar=<Tab>]])
+vim.cmd([[let mapleader=" "]])
 
 -- Exit insert mode in nvim terminal
-utils.map('t', '<leader><Esc>', '<c-\\><c-n>', opts)
-
-
-
+utils.map("t", "<leader><Esc>", "<c-\\><c-n>", opts)
