c58d3aeeb4a43fc92a7f397ff1bf7b74ebfd620f
Andrea Finocchio <finocchio.andrea94@gmail.com>
2022-12-03 12:35:04 +0000

    dap ui back to basic setup

--- lua/config/nvim-dap-ui.lua
+++ lua/config/nvim-dap-ui.lua
@@ -1,66 +1,66 @@
 require("dapui").setup({
-  icons = { expanded = "▾", collapsed = "▸" },
-  mappings = {
-    -- Use a table to apply multiple mappings
-    expand = { "l", "<CR>", "<2-LeftMouse>" },
-    open = "o",
-    remove = "d",
-    edit = "e",
-    repl = "r",
-    toggle = "t",
-  },
-  -- Expand lines larger than the window
-  -- Requires >= 0.7
-  expand_lines = vim.fn.has("nvim-0.7"),
+	icons = { expanded = "▾", collapsed = "▸" },
+	mappings = {
+		-- Use a table to apply multiple mappings
+		expand = { "l", "<CR>", "<2-LeftMouse>" },
+		open = "o",
+		remove = "d",
+		edit = "e",
+		repl = "r",
+		toggle = "t",
+	},
+	-- Expand lines larger than the window
+	-- Requires >= 0.7
+	expand_lines = vim.fn.has("nvim-0.7"),
 
 	-- Layouts define sections of the screen to place windows.
-  -- The position can be "left", "right", "top" or "bottom".
-  -- The size specifies the height/width depending on position.
-  -- Elements are the elements shown in the layout (in order).
-  -- Layouts are opened in order so that earlier layouts take priority in window sizing.
-  layouts = {
-    {
-      elements = {
-      -- Elements can be strings or table with id and size keys.
-        { id = "scopes", size = 0.25 },
-        { id = "breakpoints", size = 0.25 },
-        { id = "stacks", size = 0.25 },
-        { id = "watches", size = 0.25 },
-      },
-      size = 10,
-      position = "bottom",
-    },
-    --{
-    --  elements = {
-    --    --"repl",
-    --    --"console"
-    --  },
-    --  size = 10,
-    --  position = "bottom",
-    --},
-  },
-  floating = {
-    max_height = nil, -- These can be integers or a float between 0 and 1.
-    max_width = nil, -- Floats will be treated as percentage of your screen.
-    border = "rounded", -- Border style. Can be "single", "double" or "rounded"
-    mappings = {
-      close = { "q", "<Esc>" },
-    },
-  },
-  windows = { indent = 1 },
-  render = {
-    max_type_length = 10, -- Can be integer or nil.
-  }
+	-- The position can be "left", "right", "top" or "bottom".
+	-- The size specifies the height/width depending on position.
+	-- Elements are the elements shown in the layout (in order).
+	-- Layouts are opened in order so that earlier layouts take priority in window sizing.
+	layouts = {
+		{
+			elements = {
+				-- Elements can be strings or table with id and size keys.
+				{ id = "scopes", size = 0.25 },
+				{ id = "breakpoints", size = 0.25 },
+				{ id = "stacks", size = 0.25 },
+				{ id = "watches", size = 0.25 },
+			},
+			size = 0.20,
+			position = "left",
+		},
+		{
+			elements = {
+				"repl",
+				"console",
+			},
+			size = 0.20,
+			position = "bottom",
+		},
+	},
+	floating = {
+		max_height = nil, -- These can be integers or a float between 0 and 1.
+		max_width = nil, -- Floats will be treated as percentage of your screen.
+		border = "rounded", -- Border style. Can be "single", "double" or "rounded"
+		mappings = {
+			close = { "q", "<Esc>" },
+		},
+	},
+	windows = { indent = 1 },
+	render = {
+		max_type_length = 10, -- Can be integer or nil.
+	},
 })
 
 -- open and close with dap
 local dap, dapui = require("dap"), require("dapui")
 dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
+	dapui.open()
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
+	dapui.close()
 end
 dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
+	dapui.close()
 end
--- lua/config/nvim-dap.lua
+++ lua/config/nvim-dap.lua
@@ -1,121 +1,118 @@
-local opts = { noremap=true, silent=true }
-local utils = require('utils')
-local dap = require('dap')
+local opts = { noremap = true, silent = true }
+local utils = require("utils")
+local dap = require("dap")
 local api = vim.api
 
 -- keymaps only active when dap session is active
 local keymap_restore = {}
-dap.listeners.after['event_initialized']['me'] = function()
-  for _, buf in pairs(api.nvim_list_bufs()) do
-    local keymaps = api.nvim_buf_get_keymap(buf, 'n')
-    for _, keymap in pairs(keymaps) do
-      if keymap.lhs == "<C-k>" then
-        table.insert(keymap_restore, keymap)
-        api.nvim_buf_del_keymap(buf, 'n', '<C-k>')
-      end
-    end
-  end
-  api.nvim_set_keymap('n', '<C-k>', '<cmd>lua require("dapui").eval()<cr>', opts)
+dap.listeners.after["event_initialized"]["me"] = function()
+	for _, buf in pairs(api.nvim_list_bufs()) do
+		local keymaps = api.nvim_buf_get_keymap(buf, "n")
+		for _, keymap in pairs(keymaps) do
+			if keymap.lhs == "<C-k>" then
+				table.insert(keymap_restore, keymap)
+				api.nvim_buf_del_keymap(buf, "n", "<C-k>")
+			end
+		end
+	end
+	api.nvim_set_keymap("n", "<C-k>", '<cmd>lua require("dapui").eval()<cr>', opts)
 
-  for _, buf in pairs(api.nvim_list_bufs()) do
-    local keymaps = api.nvim_buf_get_keymap(buf, 'v')
-    for _, keymap in pairs(keymaps) do
-      if keymap.lhs == "<C-k>" then
-        table.insert(keymap_restore, keymap)
-        api.nvim_buf_del_keymap(buf, 'v', '<C-k>')
-      end
-    end
-  end
-  api.nvim_set_keymap('v', '<C-k>', '<cmd>lua require("dapui").eval()<cr>', opts)
-
+	for _, buf in pairs(api.nvim_list_bufs()) do
+		local keymaps = api.nvim_buf_get_keymap(buf, "v")
+		for _, keymap in pairs(keymaps) do
+			if keymap.lhs == "<C-k>" then
+				table.insert(keymap_restore, keymap)
+				api.nvim_buf_del_keymap(buf, "v", "<C-k>")
+			end
+		end
+	end
+	api.nvim_set_keymap("v", "<C-k>", '<cmd>lua require("dapui").eval()<cr>', opts)
 end
 
-dap.listeners.after['event_terminated']['me'] = function()
-  for _, keymap in pairs(keymap_restore) do
-    api.nvim_buf_set_keymap(
-      keymap.buffer,
-      keymap.mode,
-      keymap.lhs,
-      keymap.rhs,
-      { silent = keymap.silent == 1 }
-    )
-  end
-  keymap_restore = {}
+dap.listeners.after["event_terminated"]["me"] = function()
+	for _, keymap in pairs(keymap_restore) do
+		api.nvim_buf_set_keymap(keymap.buffer, keymap.mode, keymap.lhs, keymap.rhs, { silent = keymap.silent == 1 })
+	end
+	keymap_restore = {}
 end
 
-utils.map('n',  '<leader>dc',  '<cmd>DapContinue<cr>', opts)
-utils.map('n',  '<leader>dC',  '<cmd>lua require("dap").reverse_continue()<cr>', opts)
-utils.map('n',  '<leader>dl',  '<cmd>lua require("dap").run_last()<cr>', opts)
-utils.map('n',  '<leader>dT',  '<cmd>DapTerminate<cr>', opts)
+utils.map("n", "<leader>dc", "<cmd>DapContinue<cr>", opts)
+utils.map("n", "<leader>dC", '<cmd>lua require("dap").reverse_continue()<cr>', opts)
+utils.map("n", "<leader>dl", '<cmd>lua require("dap").run_last()<cr>', opts)
+utils.map("n", "<leader>dT", "<cmd>DapTerminate<cr>", opts)
 
-utils.map('n',  '<leader>db',	 '<cmd>DapToggleBreakpoint<cr>', opts)
-utils.map('n',  '<leader>dB',	 '<cmd>lua require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))<cr>', opts)
-utils.map('n',  '<leader>dL',	 '<cmd>lua require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<cr>', opts)
+utils.map("n", "<leader>db", "<cmd>DapToggleBreakpoint<cr>", opts)
+utils.map("n", "<leader>dB", '<cmd>lua require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))<cr>', opts)
+utils.map(
+	"n",
+	"<leader>dL",
+	'<cmd>lua require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<cr>',
+	opts
+)
 
-
-utils.map('n',  '<leader>dr',  '<cmd>DapToggleRepl<cr>', opts)
-utils.map('n',  '<leader>dl',  '<cmd>lua require("dap").run_last()<cr>', opts)
+utils.map("n", "<leader>dr", "<cmd>DapToggleRepl<cr>", opts)
+utils.map("n", "<leader>dl", '<cmd>lua require("dap").run_last()<cr>', opts)
 
 -- Python
-utils.map('n',  '<leader>dtm', ':lua require("dap-python").test_method()<cr>', opts)
-utils.map('n',  '<leader>dtc', ':lua require("dap-python").test_class()<cr>', opts)
-utils.map('v',  '<leader>dc' , ':lua require("dap-python").debug_selection()<cr>', opts)
+utils.map("n", "<leader>dtm", ':lua require("dap-python").test_method()<cr>', opts)
+utils.map("n", "<leader>dtc", ':lua require("dap-python").test_class()<cr>', opts)
+utils.map("v", "<leader>dc", ':lua require("dap-python").debug_selection()<cr>', opts)
 
 --USER = vim.fn.expand('$USER')
 
 local path_to_lldb = ""
 
 if vim.fn.has("mac") == 1 then
-    path_to_lldb = '/usr/local/Cellar/llvm/14.0.6/bin/lldb-vscode'
-		utils.map('n',  '<F6>', '<cmd>lua require("dap").step_back()<cr>', opts)
-		utils.map('n',  '<F7>', '<cmd>DapStepOut<cr>', opts)
-		utils.map('n',  '<F8>', '<cmd>DapStepOver<cr>', opts)
-		utils.map('n',  '<F9>', '<cmd>DapStepInto<cr>', opts)
-		utils.map('n',  '<F10>','<cmd>lua require("dap").run_to_cursor()<cr>', opts)
+	path_to_lldb = "/usr/local/Cellar/llvm/14.0.6/bin/lldb-vscode"
+	utils.map("n", "<F6>", '<cmd>lua require("dap").step_back()<cr>', opts)
+	utils.map("n", "<F7>", "<cmd>DapStepOut<cr>", opts)
+	utils.map("n", "<F8>", "<cmd>DapStepOver<cr>", opts)
+	utils.map("n", "<F9>", "<cmd>DapStepInto<cr>", opts)
+	utils.map("n", "<F10>", '<cmd>lua require("dap").run_to_cursor()<cr>', opts)
 elseif vim.fn.has("unix") == 1 then
-    path_to_lldb =  '/usr/bin/lldb-vscode'
-		utils.map('n',  '<F3>', '<cmd>lua require("dap").step_back()<cr>', opts)
-		utils.map('n',  '<F4>', '<cmd>DapStepOut<cr>', opts)
-		utils.map('n',  '<F5>', '<cmd>DapStepOver<cr>', opts)
-		utils.map('n',  '<F6>', '<cmd>DapStepInto<cr>', opts)
-		utils.map('n',  '<F7>', '<cmd>lua require("dap").run_to_cursor()<cr>', opts)
+	path_to_lldb = "/usr/bin/lldb-vscode"
+	utils.map("n", "<F3>", '<cmd>lua require("dap").step_back()<cr>', opts)
+	utils.map("n", "<F4>", "<cmd>DapStepOut<cr>", opts)
+	utils.map("n", "<F5>", "<cmd>DapStepOver<cr>", opts)
+	utils.map("n", "<F6>", "<cmd>DapStepInto<cr>", opts)
+	utils.map("n", "<F7>", '<cmd>lua require("dap").run_to_cursor()<cr>', opts)
 else
-    print("can't find a lldb installation check nvim-dap.lua config.")
+	print("can't find a lldb installation check nvim-dap.lua config.")
 end
 
 dap.adapters.lldb = {
-  name = 'lldb',
-  type = 'executable',
-  command = path_to_lldb, -- adjust as needed, must be absolute path
+	name = "lldb",
+	type = "executable",
+	command = path_to_lldb, -- adjust as needed, must be absolute path
 }
 dap.configurations.cpp = {
-  {
-    name = 'Launch',
-    type = 'lldb',
-    request = 'launch',
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.expand("%:r") .. '.o', 'file')
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
+	{
+		name = "Launch",
+		type = "lldb",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.expand("%:r") .. ".o", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		stopOnEntry = false,
 		args = function()
-      local argument_string = vim.fn.input('Program arguments: ')
-      return vim.fn.split(argument_string, " ", true)
-    end,
+			local argument_string = vim.fn.input("Program arguments: ")
+			return vim.fn.split(argument_string, " ", true)
+		end,
 
-    -- 💀
-    -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
-    --
-    --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-    --
-    -- Otherwise you might get the following error:
-    --
-    --    Error on launch: Failed to attach to the target process
-    --
-    -- But you should be aware of the implications:
-    -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
-    -- runInTerminal = false,
-  },
+		-- 💀
+		-- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+		--
+		--    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+		--
+		-- Otherwise you might get the following error:
+		--
+		--    Error on launch: Failed to attach to the target process
+		--
+		-- But you should be aware of the implications:
+		-- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+		-- runInTerminal = false,
+	},
 }
 
 -- If you want to use this for Rust and C, add something like this:
@@ -124,6 +121,6 @@ dap.configurations.c = dap.configurations.cpp
 dap.configurations.rust = dap.configurations.cpp
 
 -- Python
-require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
+require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
 -- change the test runner (unittest is default)
 --require('dap-python').test_runner = 'pytest' -- or django
