e20db314f685241fd890a18ff3301e5c0cf57d68
Andrea Finocchio <finocchio.andrea94@gmail.com>
2022-12-03 16:09:00 +0000

    lsp commands fix and new

--- lua/lsp_lua/init.lua
+++ lua/lsp_lua/init.lua
@@ -19,10 +19,12 @@ local on_attach = function(client, bufnr)
 	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
 	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
 	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	buf_set_keymap("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
 	buf_set_keymap("n", "<leader>a", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 	buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 	buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-	buf_set_keymap("n", "E", "<cmd>lua vim.lsp.diagnostic.open_float()<CR>", opts)
+	buf_set_keymap("n", "E", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
 
 	if client.server_capabilities.documentFormattingProvider then
 		vim.api.nvim_command([[augroup Format]])
