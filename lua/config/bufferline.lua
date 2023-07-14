c015c32a96b9a088d7e1d736ee9eb655d1816c8d
Finocchio, Andrea <afinocchio@illumina.com>
2023-07-14 15:22:13 +0100

    Fix icons

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -5,12 +5,6 @@ require("bufferline").setup({
 		right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
 		left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
 		middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
-		--indicator_icon = { style = "icon", icon = "▎" },
-		buffer_close_icon = "",
-		modified_icon = "●",
-		close_icon = "",
-		left_trunc_marker = "",
-		right_trunc_marker = "",
 		max_name_length = 400,
 		max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
 		tab_size = 28,
