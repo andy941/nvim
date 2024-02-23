be73588bea8a4a4d7ea24164521b3a8cb5e8e61b
andy941 <finocchio.andrea94@gmail.com>
2024-02-23 22:08:11 +0000

    comment out shortmess in completion

--- lua/config/completion.lua
+++ lua/config/completion.lua
@@ -1,7 +1,7 @@
 local utils = require("utils")
 
 utils.opt("o", "completeopt", "menu,menuone,noselect")
-vim.cmd([[set shortmess+=c]])
+-- vim.cmd([[set shortmess+=c]])
 vim.g.completion_confirm_key = ""
 vim.g.completion_matching_strategy_list = { "exact", "substring", "fuzzy" } -- <Tab> to navigate the completion menu
 
