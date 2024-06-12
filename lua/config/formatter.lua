1a57b4cae48eabb34a7942486be8fc3fc057bc37
Finocchio, Andrea <afinocchio@illumina.com>
2024-06-12 14:12:26 +0100

    Fix cbfmt

--- lua/config/formatter.lua
+++ lua/config/formatter.lua
@@ -34,7 +34,7 @@ cbFmt = function()
 			"markdown",
 			"--write",
 			"--best-effort",
-			"--stdin-filepath",
+			"<",
 			util.escape_path(util.get_current_buffer_file_path()),
 			"|",
 			"sed 's/^# |/#|/'",
