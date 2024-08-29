0cf70e20982178040566fb5cf684b6bddeda9f94
Finocchio, Andrea <afinocchio@illumina.com>
2024-08-29 20:05:00 +0100

    Gitsigns keymaps

--- lua/config/fugitive.lua
+++ lua/config/fugitive.lua
@@ -1,4 +1,5 @@
-local utils = require('utils')
-utils.map('n', '<leader>dv', '<cmd>Gvdiffsplit!<CR>')  -- Git diff
-utils.map('n', '<leader>hh', '<cmd>diffget //2<CR>')   -- Git diffget left
-utils.map('n', '<leader>ll', '<cmd>diffget //3<CR>')   -- Git diffget right
+local utils = require("utils")
+utils.map("n", "<leader>dv", "<cmd>Gvdiffsplit!<CR>") -- Git diff
+utils.map("n", "<leader>hh", "<cmd>diffget //2<CR>") -- Git diffget left
+utils.map("n", "<leader>ll", "<cmd>diffget //3<CR>") -- Git diffget right
+utils.map("n", "<leader>gB", "<cmd>Git blame<CR>")
