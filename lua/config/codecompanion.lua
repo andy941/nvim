2cb380cd9bcddbc151570185bb1784e1e890eca2
Finocchio, Andrea <afinocchio@illumina.com>
2024-09-19 19:11:12 +0100

    Codecompanion improvement

--- lua/config/codecompanion.lua
+++ lua/config/codecompanion.lua
@@ -10,25 +10,13 @@ require("codecompanion").setup({
 			adapter = "ollama",
 		},
 	},
-  adapters = {
-    ollama = function()
-      return require("codecompanion.adapters").extend("ollama", {
-        name = "ollama", -- Give this adapter a different name to differentiate it from the default ollama adapter
-        schema = {
-          num_ctx = {
-            default = 4096,
-          },
-        },
-      })
-    end,
-  },
 })
 
 vim.api.nvim_set_keymap("n", "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<LocalLeader>a", "<cmd>CodeCompanionToggle<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<LocalLeader>a", "<cmd>CodeCompanionToggle<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "CA", "<cmd>CodeCompanionAdd<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<LocalLeader>A", "<cmd>CodeCompanionAdd<cr>", { noremap = true, silent = true })
 
 -- Expand 'cc' into 'CodeCompanion' in the command line
 vim.cmd([[cab cc CodeCompanion]])
