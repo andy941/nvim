74ff3531d7e7310a5971a797c32d61eaecd3172e
Andrea Finocchio <finocchio.andrea94@gmail.com>
2025-12-19 16:57:14 +0000

    clean useless code

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -7,14 +7,7 @@ return {
 	},
 	config = function()
 		require("noice").setup({
-			popupmenu = { backend = "cmp" },
 			lsp = {
-				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-				},
 				message = {
 					-- Messages shown by lsp servers
 					enabled = true,
