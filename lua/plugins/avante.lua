5efad6eb16c6704914862cbffd05e6e9360471fd
Finocchio, Andrea <afinocchio@illumina.com>
2026-02-24 11:01:25 -0800

    Move to sidekick

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,42 +1,42 @@
 return {
-	{
-		"yetone/avante.nvim",
-		event = "VeryLazy",
-		version = false,
-		build = "make",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			"nvim-telescope/telescope.nvim",
-			"stevearc/dressing.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"nvim-treesitter/nvim-treesitter",
-			-- "zbirenbaum/copilot.lua",
-			{
-				"MeanderingProgrammer/render-markdown.nvim",
-				ft = { "Avante" },
-			},
-		},
-		opts = {
-			provider = "copilot",
-			mode = "agentic",
-			providers = {
-				copilot = {
-					model = "gpt-5-mini",
-					auto_select_model = false,
-				},
-			},
-			hints = { enabled = true },
-			selector = {
-				provider = "telescope",
-			},
-			dual_boost = { enabled = false },
-			behaviour = {
-				auto_suggestions = false,
-				enable_token_counting = true,
-				enable_fastapply = false,
-				auto_add_current_file = false,
-			},
-		},
-	},
+	-- {
+	-- 	"yetone/avante.nvim",
+	-- 	event = "VeryLazy",
+	-- 	version = false,
+	-- 	build = "make",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"MunifTanjim/nui.nvim",
+	-- 		"nvim-telescope/telescope.nvim",
+	-- 		"stevearc/dressing.nvim",
+	-- 		"nvim-tree/nvim-web-devicons",
+	-- 		"nvim-treesitter/nvim-treesitter",
+	-- 		-- "zbirenbaum/copilot.lua",
+	-- 		{
+	-- 			"MeanderingProgrammer/render-markdown.nvim",
+	-- 			ft = { "Avante" },
+	-- 		},
+	-- 	},
+	-- 	opts = {
+	-- 		provider = "copilot",
+	-- 		mode = "agentic",
+	-- 		providers = {
+	-- 			copilot = {
+	-- 				model = "gpt-5-mini",
+	-- 				auto_select_model = false,
+	-- 			},
+	-- 		},
+	-- 		hints = { enabled = true },
+	-- 		selector = {
+	-- 			provider = "telescope",
+	-- 		},
+	-- 		dual_boost = { enabled = false },
+	-- 		behaviour = {
+	-- 			auto_suggestions = false,
+	-- 			enable_token_counting = true,
+	-- 			enable_fastapply = false,
+	-- 			auto_add_current_file = false,
+	-- 		},
+	-- 	},
+	-- },
 }
